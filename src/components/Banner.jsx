3ebda371046ce112d87c5aaadb57deedda3ce918
MarcelDurgante <marceldur@gmail.com>
2023-07-28 04:08:34 +0200

    Organized files

---
+++ src/components/Banner.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Banner = () => {
+  return (
+    <header>
+      <div>
+        <img src='./GloboLogo.png' alt='Globomatic logo' />
+      </div>
+      <div>
+        Providing houses all over the world.
+      </div>
+    </header>
+  )
+}
+
+export default Banner;
