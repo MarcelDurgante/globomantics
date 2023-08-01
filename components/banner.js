e967262c61698b2899ad4d2928112eb1b6435a7b
MarcelDurgante <marceldur@gmail.com>
2023-08-01 07:52:35 +0200

    Moved styles to css file

--- components/banner.js
+++ components/banner.js
@@ -1,18 +1,12 @@
 import { logo } from "./banner.module.css";
 
-const subtitleStyle = {
-    fontStyle: "italic",
-    fontSize: "x-large",
-    color: "coral",
-};
-
 const Banner = ({ children }) => {
     return (
         <header className="row mb-4">
             <div className="col-5">
                 <img src="./GloboLogo.png" alt="logo" className={logo} />
             </div>
-            <div className="col-7 mt-5" style={subtitleStyle}>
+            <div className="col-7 mt-5 subtitleStyle">
                 {children}
             </div>
         </header>
