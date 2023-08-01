fc3e0009879701724780b242f1ea8e4dea8d6049
MarcelDurgante <marceldur@gmail.com>
2023-08-01 05:23:10 +0200

    added Bootstrap link

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,18 +1,7 @@
-import { Html, Head, Main, NextScript } from 'next/document';
-
 import Banner from '@/components/Banner';
 
 export default function Home() {
   return (
-    <Html>
-      <Head />
-      <link href='"https://cdn' />
-      <body>
-        <Main />
-        <NextScript />
-        <Banner />
-      </body>
-    </Html>
+    <Banner />
   )
-
 };
---
+++ src/pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+    return (
+        <Html>
+            <Head>
+                <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css' rel='stylesheet' />
+                {/* eslint-disable-next-line @next/next/no-css-tags */}
+                <link rel='stylesheet' href='../app/globals.css' />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+
+};
