66be8b196e4ffb74724f32f344fc352b96206de7
MarcelDurgante <marceldur@gmail.com>
2023-08-01 08:25:43 +0200

    Implemented dynamic text by passing props from app to Banner

--- components/app.js
+++ components/app.js
@@ -1,11 +1,10 @@
 import Banner from "./banner";
 
 const App = () => {
+
     return (
         <>
-            <Banner>
-                <div>Providing houses all over the world</div>
-            </Banner>
+            <Banner headerText='Providing houses all over the world' />
         </>
     );
 };
--- components/banner.js
+++ components/banner.js
@@ -1,13 +1,16 @@
 import { logo } from "./banner.module.css";
 
-const Banner = ({ children }) => {
+const Banner = (props) => {
+
+    const { headerText } = props
+
     return (
         <header className="row mb-4">
             <div className="col-5">
                 <img src="./GloboLogo.png" alt="logo" className={logo} />
             </div>
             <div className="col-7 mt-5 subtitleStyle">
-                {children}
+                {headerText}
             </div>
         </header>
     );
