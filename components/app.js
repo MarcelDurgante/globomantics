86f349293ed81ad55e1be46cb125038d7e87cfa4
MarcelDurgante <marceldur@gmail.com>
2023-08-01 13:02:04 +0200

    Created HoseList component using map built in function

--- components/app.js
+++ components/app.js
@@ -1,10 +1,12 @@
 import Banner from "./banner";
+import HouseList from './houseList';
 
 const App = () => {
 
     return (
         <>
             <Banner headerText='Providing houses all over the world' />
+            <HouseList />
         </>
     );
 };
