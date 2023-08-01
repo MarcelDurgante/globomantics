f19de594b88a2fbc8069494782af57764efabcee
MarcelDurgante <marceldur@gmail.com>
2023-08-01 04:45:38 +0200

    Removed Tailwind

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
 import Banner from '@/components/Banner';
 
 export default function Home() {
-  return <Banner />
+  return (
+    <Html>
+      <Head />
+      <link href='"https://cdn' />
+      <body>
+        <Main />
+        <NextScript />
+        <Banner />
+      </body>
+    </Html>
+  )
+
 };
