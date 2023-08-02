c52ce67e75956da2b94096a2e13dcc7492dcbaa0
MarcelDurgante <marceldur@gmail.com>
2023-08-02 14:18:54 +0200

    Refactor conditional td render using template literal

--- components/houseRow.js
+++ components/houseRow.js
@@ -2,18 +2,11 @@ import React from 'react';
 import currencyFormatter from '@/helpers/currencyFormatter';
 // distructuring prop house in a deper level
 const HouseRow = ({ house }) => {
-    let priceTd;
-    if (house.price < 500000) {
-        priceTd = <td>{currencyFormatter.format(house.price)}</td>;
-    } else {
-        priceTd = <td className='text-primary'>{currencyFormatter.format(house.price)} </td>
-    }
-
     return (
         <tr>
             <td>{house.address}</td>
             <td>{house.country}</td>
-            {priceTd}
+            <td className={`${house.price >= 500000 ? 'text-primary' : ''}`}>{currencyFormatter.format(house.price)}</td>
         </tr>
     )
 }
